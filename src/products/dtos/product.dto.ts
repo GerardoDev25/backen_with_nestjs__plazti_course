caa62016749724b5be82749c9e03dd5f379e534f
Gerardo Miranda <gerardodev25@gmail.com>
2022-12-11 17:54:51 -0400

    Integrando Swagger y PartialType con Open API

--- src/products/dtos/product.dto.ts
+++ src/products/dtos/product.dto.ts
@@ -6,27 +6,32 @@ import {
   IsPositive,
 } from 'class-validator';
 
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, ApiProperty } from '@nestjs/swagger';
 
 export class CreateProductDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   readonly name: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   readonly description: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsNumber()
   @IsPositive()
   readonly price: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsNumber()
   @IsPositive()
   readonly stock: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsUrl()
   readonly image: string;
