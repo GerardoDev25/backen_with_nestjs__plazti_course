2c8cf1e610d49e910647c027f40b191c76343aba
Gerardo Miranda <gerardodev25@gmail.com>
2022-12-11 10:20:42 -0400

    useValue y useClass

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,18 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+const API_KEY = '123456789';
+const API_KEY_PRO = 'PROD_123456789';
+
 @Module({
   imports: [UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PRO : API_KEY,
+    },
+  ],
 })
 export class AppModule {}
