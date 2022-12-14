b7c67e31bde64fb61f0a3ebf8c3a7a73cfb817d6
Gerardo Miranda <gerardodev25@gmail.com>
2022-12-14 17:51:58 -0400

    11 - Conectando Passport con JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 import { AuthService } from './services/auth.service';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './strategies/local.strategy';
 import { AuthController } from './controller/auth.controller';
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [UsersModule, PassportModule, JwtModule],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
