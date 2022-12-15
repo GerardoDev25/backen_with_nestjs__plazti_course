a720529787c262e39333201a7085c5392b17c873
Gerardo Miranda <gerardodev25@gmail.com>
2022-12-15 18:48:48 -0400

    06 - Integración de node-postgres con NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { HttpModule, HttpService } from '@nestjs/axios';
 import * as joi from 'joi';
+import { Client } from 'pg';
 
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
@@ -13,6 +14,18 @@ import { AppService } from './app.service';
 
 import config from './config/config';
 import { enviroments } from './config/enviroment';
+
+const client = new Client({
+  user: 'root',
+  host: 'localhost',
+  database: 'my_db',
+  password: '123456',
+  port: 5432,
+});
+client.connect();
+client.query('SELECT * FROM task', (err, res) => {
+  err ? console.error(err) : console.log(res.rows);
+});
 
 @Module({
   imports: [
@@ -35,16 +48,16 @@ import { enviroments } from './config/enviroment';
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: 'TASKS',
-      useFactory: async (http: HttpService) => {
-        const task = await http
-          .get('https://jsonplaceholder.typicode.com/todos')
-          .toPromise();
-        return task.data;
-      },
-      inject: [HttpService],
-    },
+    // {
+    //   provide: 'TASKS',
+    //   useFactory: async (http: HttpService) => {
+    //     const task = await http
+    //       .get('https://jsonplaceholder.typicode.com/todos')
+    //       .toPromise();
+    //     return task.data;
+    //   },
+    //   inject: [HttpService],
+    // },
   ],
 })
 export class AppModule {}
