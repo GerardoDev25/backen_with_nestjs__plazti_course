b2e8cde7f75f763c518bde74e65b93738bdf17a0
Gerardo Miranda <gerardodev25@gmail.com>
2022-12-18 16:07:21 -0400

    10 - ruta de login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { UsersModule } from 'src/users/users.module';
 
 import { AuthService } from './services/auth.service';
 import { LocalStrategy } from './strategies/strategy';
+import { AuthController } from './controllers/auth.controller';
 
 @Module({
   imports: [UsersModule],
   providers: [AuthService, PassportModule, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
---
+++ src/auth/controllers/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+
+import { AuthService } from '../services/auth.service';
+import { User } from '../../users/entities/user.entity';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  @UseGuards(AuthGuard('local'))
+  login(@Req() req: Request) {
+    const user = req.user as User;
+    return user;
+    // return this.authService.generateJWT(user);
+  }
+}
